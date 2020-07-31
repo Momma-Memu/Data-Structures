64237c626afa1ca17cfa82262c55acf3c273309e
Miah Barnes <miahellenbarnes@gmail.com>
2020-07-31 11:40:01 -0400

    Update linked_list.js

--- LinkedLists-Stacks-Queues/data-structures-linked-list/lib/linked_list.js
+++ LinkedLists-Stacks-Queues/data-structures-linked-list/lib/linked_list.js
@@ -1,26 +1,3 @@
-// ============================================================================
-// Implementation Exercise: Singly Linked List
-// ============================================================================
-//
-// -------
-// Prompt:
-// -------
-//
-// Implement a Singly Linked List and all of its methods below!
-//
-// ------------
-// Constraints:
-// ------------
-//
-// Make sure the time and space complexity of each is equivalent to those
-// in the table provided in the Time and Space Complexity Analysis section
-// of your Linked List reading!
-//
-// -----------
-// Let's Code!
-// -----------
-
-// TODO: Implement a Linked List Node class here
 class Node {
     constructor(val) {
         this.value = val;
