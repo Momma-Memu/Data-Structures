c2a42111ca279eb3f7ee7308fb452078571a169b
Miah Barnes <miahellenbarnes@gmail.com>
2020-07-31 11:39:28 -0400

    Update stack.js

--- LinkedLists-Stacks-Queues/data-structures-stack/lib/stack.js
+++ LinkedLists-Stacks-Queues/data-structures-stack/lib/stack.js
@@ -1,25 +1,3 @@
-// ============================================================================
-// Implementation Exercise: Stack
-// ============================================================================
-//
-// -------
-// Prompt:
-// -------
-//
-// Implement a Stack and all of its methods below!
-//
-// ------------
-// Constraints:
-// ------------
-//
-// Make sure the time and space complexity of each is equivalent to those
-// in the table provided in the Time and Space Complexity Analysis section
-// of your Stack reading!
-//
-// -----------
-// Let's Code!
-// -----------
-
 class Node {
     constructor(val) {
         this.value = val;
